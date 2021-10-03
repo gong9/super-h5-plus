91e02b0a748b351772aa64f8f3d5d708a30e8d46
“gongzhen” <gong1782523482@163.com>
2021-10-03 11:16:57 +0800

    fix: bugfix

--- packages/super-editor/src/components/compConfigForm/color/index.tsx
+++ packages/super-editor/src/components/compConfigForm/color/index.tsx
@@ -4,10 +4,9 @@
  */
 
 import { FC, useState, useEffect } from 'react';
-import { Input } from 'antd';
 import { SketchPicker, ColorResult } from 'react-color';
 import classnames from 'classnames';
-import { color2rgba, rgbaObj2string } from '@/util/util';
+import { color2rgba, rgbaObj2string, checkIn } from '@/util/util';
 
 import './index.less';
 
@@ -17,20 +16,43 @@ const SuperColor: FC<SuperColorProps> = () => {
   const [color, setColor] = useState<string>(() => color2rgba('000000', 1));
   const [hideSeletor, setHideSeletor] = useState(true);
 
+  useEffect(() => {
+    document
+      .querySelector('.editor-body-right')
+      ?.addEventListener('click', hideSeletorHandle);
+
+    return () => {
+      document
+        .querySelector('.editor-body-right')
+        ?.removeEventListener('click', hideSeletorHandle);
+    };
+  }, []);
+
   const changeColor = (color: ColorResult) => {
     setColor(rgbaObj2string(color.rgb));
   };
 
+  const hideSeletorHandle = (e: any) => {
+    let isTagetNode = document
+      .querySelector('.sketch-picker')
+      ?.contains(e.target);
+
+    if (!isTagetNode) {
+      setHideSeletor(true);
+    }
+  };
+
   return (
     <div className="super-color">
-      <Input
+      <div
         className="super-color-demo"
         style={{
           background: `${color}`,
         }}
-        onFocus={() => setHideSeletor(false)}
-        onBlur={() => setHideSeletor(true)}
-      ></Input>
+        onClick={() => {
+          setHideSeletor(false);
+        }}
+      ></div>
 
       <SketchPicker
         className={classnames('super-color-seletor', { hide: hideSeletor })}
